5581c9b45d8f595ad9de3e295a26074aa1f2e8c5
Yanis Lukes <pendrokar@gmail.com>
2018-09-14 13:51:25 +0300

    non-negative value

--- src/Model/Table/DebtsTable.php
+++ src/Model/Table/DebtsTable.php
@@ -55,7 +55,7 @@ class DebtsTable extends Table
             ->notEmpty('title');
 
         $validator
-            ->integer('value')
+            ->nonNegativeInteger('value')
             ->requirePresence('value', 'create')
             ->notEmpty('value');
 
